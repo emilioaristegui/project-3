71900099bca2031ce1f36874a6476838ebc08f2d
pyunes <128106993+pyunes@users.noreply.github.com>
2023-07-11 18:50:51 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+# Import the dependencies.
+import numpy as np
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+
+# create engine to hawaii.sqlite
+engine = sqlalchemy.create_engine('postgresql:///data/db.db')
+# reflect an existing database into a new model
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route('/data')
+def return_data(): 
+    results=engine.execute('select * from data').fetchall()
+    #co2=[]
+    #for each_result in results: 
+     #   collision = dict(each_result)
+      #  co2.append(collision)
+    #return jsonify(co2)
+
+
+@app.route('/')
+def welcome():
+    return(
+        f"Weclome to the CO2 Global Pollution API!<br/>"
+        f"<br/>"
+        f"Please find below all the available Routes:<br/>"
+    )
