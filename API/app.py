d1d458af016e1d914481a02ff30efe72f9b4a4b6
RafaOB95 <127167255+RafaOB95@users.noreply.github.com>
2023-07-14 00:37:15 -0600

    Código con archivo json

--- API/app.py
+++ API/app.py
@@ -1,6 +1,6 @@
 # Import the dependencies.
-import pandas as pd
 import numpy as np
+import pandas as pd
 import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -10,12 +10,15 @@ from flask import Flask, render_template
 import json
 
 
+
 #################################################
 # Database Setup
 #################################################
 
 # Create database connection
-connection_string= 'postgresql+psycopg2://postgres:1102@localhost:5433/Data'
+
+#connection_string= 'postgresql+psycopg2://postgres:1102@localhost:5433/Data'
+connection_string= 'postgresql+psycopg2://postgres:1234@localhost:5432/project_3'
 engine = create_engine(connection_string)
 
 #################################################
@@ -41,16 +44,22 @@ def return_data():
     results = {
         'country': results['country'].to_list(),
         'region': results['region'].to_list(),
+        'emissions': results['emissions'].to_list(),
+        'industry': results['industry'].to_list(),
+        'segment': results['segment'].to_list(),
+        'reason': results['reason'].to_list(),
+        'baseYear': results['base_year'].to_list(),
+        'ISO': results['iso_3166_country_code'].to_list(),
+        'Lat': results['latitude'].to_list(),
+        'Lon': results['longitude'].to_list(),
+        
     }
+    df = pd.DataFrame(results, columns = ['region', 'country','emissions','industry','segment','reason','baseYear','ISO','Lat','Lon'])
+    df.to_json('static/merged.json')
     return results
-
-# dataframe to json
-#@app.route('/json')
-#def return_json():
-#df2 = results.to_json(orient = 'index')
-#return df2
 
 if __name__ == '__main__':
     app.run(debug=False)
+
 
 
--- API/static/logic.js
+++ API/static/logic.js
@@ -1,1 +1,17 @@
-console.log("whatever")
+//console.log("whatever")
+
+// Create the trace
+
+// Aquí estoy seguro de que algo está mal, porque se genera un bucle infinito y no carga el plot
+// hbaría que revisar si el url está bien o algo...
+d3.json('http://127.0.0.1:5000/data').then(d=>{
+    let trace1={
+        x:d.country,
+        y:d.region,
+        type:'bar'
+    }
+    
+    let data = [trace1]
+    
+    Plotly.newPlot('plot', data)
+})
