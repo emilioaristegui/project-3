e0ab702f2f89c435127d5e473fb707ecf5b5cd11
pyunes <128106993+pyunes@users.noreply.github.com>
2023-07-15 13:58:13 -0600

    Add files via upload

--- API/app.py
+++ API/app.py
@@ -3,11 +3,12 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
+#from sqlalchemy.ext.automap import automap_base
+#from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, render_template
 import json
+#import plotly.express as px
 
 
 
@@ -18,7 +19,7 @@ import json
 # Create database connection
 
 #connection_string= 'postgresql+psycopg2://postgres:1102@localhost:5433/Data'
-connection_string= 'postgresql+psycopg2://postgres:1234@localhost:5432/project_3'
+connection_string= 'postgresql+psycopg2://postgres:postgres@localhost:5433/co2_database'
 engine = create_engine(connection_string)
 
 #################################################
@@ -40,6 +41,7 @@ def welcome():
 @app.route('/data')
 def return_data(): 
     
+    #reading SQL database, creating a pandas df for easier manipulation
     results = pd.read_sql('select * from merged', engine)
     results = {
         'country': results['country'].to_list(),
@@ -55,11 +57,31 @@ def return_data():
         
     }
     df = pd.DataFrame(results, columns = ['region', 'country','emissions','industry','segment','reason','baseYear','ISO','Lat','Lon'])
+    #pandas df converted into json file
     df.to_json('static/merged.json')
+    return results
+
+@app.route('/prueba')
+def return_data2(): 
+    
+    results = pd.read_sql('select * from merged', engine)
+    results = {
+        'country': results['country'].to_list(),
+        'region': results['region'].to_list(),
+        'emissions': results['emissions'].to_list(),
+        'industry': results['industry'].to_list(),
+        'segment': results['segment'].to_list(),
+        'reason': results['reason'].to_list(),
+        'baseYear': results['base_year'].to_list(),
+        'ISO': results['iso_3166_country_code'].to_list(),
+        'Lat': results['latitude'].to_list(),
+        'Lon': results['longitude'].to_list(),
+        
+    }
+    df = pd.DataFrame(results, columns = ['region', 'country','emissions','industry','segment','reason','baseYear','ISO','Lat','Lon'])
+    year = df.groupby(["country"])["industry"].count()
+    year.to_json('static/groupby.json')
     return results
 
 if __name__ == '__main__':
     app.run(debug=False)
-
-
-
